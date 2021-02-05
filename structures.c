a74bda6e03e141caf5d38620e3350c06a00d3091
Siddhesh Patkar <patkarsiddhesh2018@gmail.com>
2021-02-05 17:03:56 +0530

    new added

--- structures.c
+++ structures.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<string.h>
 
-struct student
+struct Student
 {
     char name[50];
     char major[20];
@@ -12,17 +12,18 @@ struct student
 
 int main()
 {
-    struct student student1;
+    struct Student student1;
     student1.age = 10;
     student1.gpa = 4.5;
     strcpy(student1.name,"Jim");
     strcpy(student1.major,"Business");
 
-    printf("Student Details");
-    printf("\n%c",student1.name);
-    printf("\n%c",student1.major);
+    printf("Student Details\n");
+    printf("%s",student1.name);
+    printf("\n%s",student1.major);
     printf("\n%d",student1.age);
     printf("\n%lf",student1.gpa);
 
+    
     return 0;
 }
